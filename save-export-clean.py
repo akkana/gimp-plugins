23702c8a49f1d863661a64e2f2b038f5ee2825af
Akkana Peck <akkana@shallowsky.com>
2012-08-10 21:21:36 -0700

    Save or export the current image back to the same filename, regardless of whether it's XCF or another format, and mark the image clean.
    If there's no current filename yet, prompt for one, and take a
    guess at a reasonable directory to use to save it.

---
+++ save-export-clean.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+# Save or export the current image -- do the right thing whether it's
+# XCF (save) or any other format (export). This will mark the image clean,
+# so GIMP won't warn you when you exit.
+# Warning: this does not show a lot of extra dialogs, etc. or warn you
+# if you're about to overwrite something! Use with caution.
+
+# Copyright 2009 by Akkana Peck, http://www.shallowsky.com/software/
+# You may use and distribute this plug-in under the terms of the GPL v2
+# or, at your option, any later GPL version.
+
+from gimpfu import *
+import gtk
+import os
+import collections
+
+def python_export_clean(img, drawable) :
+    filename = img.filename
+    if not filename :
+        chooser = gtk.FileChooserDialog(title=None,
+                                        action=gtk.FILE_CHOOSER_ACTION_SAVE,
+                                        buttons=(gtk.STOCK_CANCEL,
+                                                 gtk.RESPONSE_CANCEL,
+                                                 gtk.STOCK_OPEN,
+                                                 gtk.RESPONSE_OK))
+        # Might want to set a current folder:
+        save_dir = choose_likely_save_dir()
+        if save_dir :
+            chooser.set_current_folder(save_dir)
+
+        # Oh, cool, we could have shortcuts to image folders,
+        # and maybe remove the stupid fstab shortcuts GTK adds for us.
+        #chooser.add_shortcut_folder(folder)
+        #chooser.remove_shortcut_folder(folder)
+
+        response = chooser.run()
+        if response != gtk.RESPONSE_OK:
+            return
+
+        filename = chooser.get_filename()
+        img.filename = filename
+        chooser.destroy()
+
+    pdb.gimp_file_save(img, drawable, filename, filename)
+    pdb.gimp_image_clean_all(img)
+
+def choose_likely_save_dir() :
+    # Choose the most popular path where currently open files live.
+    # (There's got to be some cleverer, more pythonic way of doing this.)
+    # Set up a dictionary of all the directory paths we've seen:
+    dirpaths = {}
+    for img in gimp.image_list() :
+        if img.filename :
+            d = os.path.dirname(img.filename)
+            if d in dirpaths.keys() :
+                dirpaths[d] += 1
+            else :
+                dirpaths[d] = 1
+
+    # Now find the dictionary entry with the biggest value
+    popular_path = None
+    uses = 0
+    for key in dirpaths.keys() :
+        if dirpaths[key] > uses :
+            uses = dirpaths[key]
+            popular_path = key
+    return popular_path
+
+register(
+        "python_fu_export_clean",
+        "Save or export the current image, then mark it clean.",
+        "Save or export the current image, then mark it clean.",
+        "Akkana Peck",
+        "Akkana Peck",
+        "20012",
+        "Save/Export clean",
+        "*",
+        [
+            (PF_IMAGE, "image", "Input image", None),
+            (PF_DRAWABLE, "drawable", "Input drawable", None),
+        ],
+        [],
+        python_export_clean,
+        menu = "<Image>/File/Save/"
+)
+
+main()
+
