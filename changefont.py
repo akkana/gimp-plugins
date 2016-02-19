990acefb07c558c9fa889d08b33b282ee1397335
Akkana Peck <akkana@shallowsky.com>
2016-02-19 10:48:20 -0700

    Allow changing font face as well as size

--- changefont.py
+++ changefont.py
@@ -2,34 +2,34 @@
 
 from gimpfu import *
 
-def python_change_font_size(img, fontsize) :
+def python_change_font(img, fontsize, fontface) :
     pdb.gimp_image_undo_group_start(img)
 
     for l in img.layers:
         if not pdb.gimp_item_is_text_layer(l):
             continue
 	pdb.gimp_text_layer_set_font_size(l, fontsize, 0)
-        # You can also change font face, like this:
-        # pdb.gimp_text_layer_set_font(l, font)
+        pdb.gimp_text_layer_set_font(l, fontface)
 
     pdb.gimp_image_undo_group_end(img)
 
 register(
-       	"python_fu_change_font_size",
-	"Change every text layer in an image to a new font size",
-	"Change every text layer in an image to a new font size",
+       	"python_fu_change_font",
+	"Change every text layer in an image to a new font and size",
+	"Change every text layer in an image to a new font and size",
 	"Akkana Peck",
 	"Akkana Peck",
 	"2016",
-	"Change Font Sizes...",
+	"Change Font in all Text Layers...",
 	"*",
 	[
           (PF_IMAGE, "image", "Input image", None),
           (PF_SPINNER, "fontsize", "New Font Size",
-           14, (1, 50, 1))
+           14, (1, 50, 1)),
+          (PF_FONT, "font", "New Font", "Sans"),
 	],
 	[],
-	python_change_font_size,
+	python_change_font,
         menu="<Image>/Filters/Generic")
 
 main()
